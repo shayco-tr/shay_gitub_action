6d75e02dba9d5b747bbe3c9c1770617cfbc0562f
Shay Cohen <shayco@etoro.com>
2021-11-04 16:18:21 +0200

    new

--- jenkins.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd /home/shahars && mkdir ./IaC && wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - && sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' && sudo apt update && sudo apt install jenkins"
